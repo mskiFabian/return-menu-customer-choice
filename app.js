739747c662b5dd424b1c93b671aadd72ddda5896
Fabian <125907927+mskiFabian@users.noreply.github.com>
2023-03-30 23:46:49 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    order: function(starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+    } 
+}
+
+console.log(restaurant.order(2, 0))
